3c06ed0c0b22ae7bf50ff068726f207beba69dd2
JarrettR <jrainier@gmail.com>
2016-03-21 19:13:42 -0700

    Added blink

---
+++ Firmware/Blink.X/main.c
@@ -0,0 +1,55 @@
+/******************************************************************************/
+/* Files to Include                                                           */
+/******************************************************************************/
+
+#if defined(__XC)
+    #include <xc.h>        /* XC8 General Include File */
+#elif defined(HI_TECH_C)
+    #include <htc.h>       /* HiTech General Include File */
+#elif defined(__18CXX)
+    #include <p18cxxx.h>   /* C18 General Include File */
+#endif
+
+#if defined(__XC) || defined(HI_TECH_C)
+
+#include <stdint.h>        /* For uint8_t definition */
+#include <stdbool.h>       /* For true/false definition */
+
+#endif
+
+#include "system.h"        /* System funct/params, like osc/peripheral config */
+#include "user.h"          /* User funct/params, such as InitApp */
+
+/******************************************************************************/
+/* User Global Variable Declaration                                           */
+/******************************************************************************/
+
+/* i.e. uint8_t <variable_name>; */
+
+/******************************************************************************/
+/* Main Program                                                               */
+/******************************************************************************/
+
+void main(void)
+{
+    /* Configure the oscillator for the device */
+    ConfigureOscillator();
+
+    /* Initialize I/O and Peripherals for application */
+    InitApp();
+
+    int i;
+    int state = 0;
+    
+    TRISBbits.TRISB7 = 0;
+
+    while(1)
+    {
+        LATBbits.LB7 = state;
+        for(i = 0; i < 500; i++)
+            __delay_ms(10);
+        state = ~state;
+    }
+
+}
+
