5549317e7a710de1f14fc16528560c513adc5bae
JarrettR <jrainier@gmail.com>
2016-03-23 20:58:45 -0700

    Keyboard demo

---
+++ Firmware/hid_keyboard/firmware/src/system_config/exp16/pic24fj256gb210_pim/system.c
@@ -0,0 +1,143 @@
+/********************************************************************
+ Software License Agreement:
+
+ The software supplied herewith by Microchip Technology Incorporated
+ (the "Company") for its PIC(R) Microcontroller is intended and
+ supplied to you, the Company's customer, for use solely and
+ exclusively on Microchip PIC Microcontroller products. The
+ software is owned by the Company and/or its supplier, and is
+ protected under applicable copyright laws. All rights are reserved.
+ Any use in violation of the foregoing restrictions may subject the
+ user to criminal sanctions under applicable laws, as well as to
+ civil liability for the breach of the terms and conditions of this
+ license.
+
+ THIS SOFTWARE IS PROVIDED IN AN "AS IS" CONDITION. NO WARRANTIES,
+ WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT NOT LIMITED
+ TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. THE COMPANY SHALL NOT,
+ IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL OR
+ CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+ *******************************************************************/
+
+#include "system.h"
+#include "system_config.h"
+#include "usb.h"
+#include "leds.h"
+#include "buttons.h"
+#include "adc.h"
+
+/** CONFIGURATION Bits **********************************************/
+
+// CONFIG4
+
+// CONFIG3
+#pragma config WPFP = WPFP255           // Write Protection Flash Page Segment Boundary (Highest Page (same as page 170))
+#pragma config SOSCSEL = SOSC           // Secondary Oscillator Power Mode Select (Secondary oscillator is in Default (high drive strength) Oscillator mode)
+#pragma config WUTSEL = LEG             // Voltage Regulator Wake-up Time Select (Default regulator start-up time is used)
+#pragma config ALTPMP = ALPMPDIS        // Alternate PMP Pin Mapping (EPMP pins are in default location mode)
+#pragma config WPDIS = WPDIS            // Segment Write Protection Disable (Segmented code protection is disabled)
+#pragma config WPCFG = WPCFGDIS         // Write Protect Configuration Page Select (Last page (at the top of program memory) and Flash Configuration Words are not write-protected)
+#pragma config WPEND = WPENDMEM         // Segment Write Protection End Page Select (Protected code segment upper boundary is at the last page of program memory; the lower boundary is the code page specified by WPFP)
+
+// CONFIG2
+#pragma config POSCMOD = HS             // Primary Oscillator Select (HS Oscillator mode is selected)
+#pragma config IOL1WAY = ON             // IOLOCK One-Way Set Enable (The IOLOCK bit (OSCCON<6>) can be set once, provided the unlock sequence has been completed. Once set, the Peripheral Pin Select registers cannot be written to a second time.)
+#pragma config OSCIOFNC = ON            // OSCO Pin Configuration (OSCO/CLKO/RC15 functions as port I/O (RC15))
+#pragma config FCKSM = CSDCMD           // Clock Switching and Fail-Safe Clock Monitor (Clock switching and Fail-Safe Clock Monitor are disabled)
+#pragma config FNOSC = PRIPLL           // Initial Oscillator Select (Primary Oscillator with PLL module (XTPLL, HSPLL, ECPLL))
+#pragma config PLL96MHZ = ON            // 96MHz PLL Startup Select (96 MHz PLL is enabled automatically on start-up)
+#pragma config PLLDIV = DIV2            // 96 MHz PLL Prescaler Select (Oscillator input is divided by 2 (8 MHz input))
+#pragma config IESO = OFF               // Internal External Switchover (IESO mode (Two-Speed Start-up) is disabled)
+
+// CONFIG1
+#pragma config WDTPS = PS32768          // Watchdog Timer Postscaler (1:32,768)
+#pragma config FWPSA = PR128            // WDT Prescaler (Prescaler ratio of 1:128)
+#pragma config ALTVREF = ALTVREDIS      // Alternate VREF location Enable (VREF is on a default pin (VREF+ on RA9 and VREF- on RA10))
+#pragma config WINDIS = OFF             // Windowed WDT (Standard Watchdog Timer enabled,(Windowed-mode is disabled))
+#pragma config FWDTEN = OFF             // Watchdog Timer (Watchdog Timer is disabled)
+#pragma config ICS = PGx2               // Emulator Pin Placement Select bits (Emulator functions are shared with PGEC2/PGED2)
+#pragma config GWRP = OFF               // General Segment Write Protect (Writes to program memory are allowed)
+#pragma config GCP = OFF                // General Segment Code Protect (Code protection is disabled)
+#pragma config JTAGEN = OFF             // JTAG Port Enable (JTAG port is disabled)
+ 
+/*********************************************************************
+* Function: void SYSTEM_Initialize( SYSTEM_STATE state )
+*
+* Overview: Initializes the system.
+*
+* PreCondition: None
+*
+* Input:  SYSTEM_STATE - the state to initialize the system into
+*
+* Output: None
+*
+********************************************************************/
+void SYSTEM_Initialize( SYSTEM_STATE state )
+{
+    switch(state)
+    {
+        case SYSTEM_STATE_USB_START:
+            //Make sure that the general purpose output driver multiplexed with
+            //the VBUS pin is always consistently configured to be tri-stated in
+            //USB applications, so as to avoid any possible contention with the host.
+            //(ex: maintain TRISFbits.TRISF7 = 1 at all times).
+            TRISFbits.TRISF7 = 1;
+
+            LED_Enable(LED_USB_DEVICE_STATE);
+            LED_Enable(LED_USB_DEVICE_HID_KEYBOARD_CAPS_LOCK);
+            
+            BUTTON_Enable(BUTTON_USB_DEVICE_HID_KEYBOARD_KEY);
+            break;
+            
+        case SYSTEM_STATE_USB_SUSPEND:
+            //If developing a bus powered USB device that needs to be USB compliant,
+            //insert code here to reduce the I/O pin and microcontroller power consumption,
+            //so that the total current is <2.5mA from the USB host's VBUS supply.
+            //If developing a self powered application (or a bus powered device where
+            //official USB compliance isn't critical), nothing strictly needs
+            //to be done during USB suspend.
+
+            USBSleepOnSuspend();
+            break;
+
+        case SYSTEM_STATE_USB_RESUME:
+            //Here would be a good place to restore I/O pins and application states
+            //back to their original values that may have been saved/changed at the 
+            //start of the suspend event (ex: when the SYSTEM_Initialize(SYSTEM_STATE_USB_SUSPEND)
+            //callback was called.
+            
+            //If you didn't change any I/O pin states prior to entry into suspend,
+            //then nothing explicitly needs to be done here.  However, by the time
+            //the firmware returns from this function, the full application should
+            //be restored into effectively exactly the same state as the application
+            //was in, prior to entering USB suspend.
+            
+            //Additionally, before returning from this function, make sure the microcontroller's
+            //currently active clock settings are compatible with USB operation, including
+            //taking into consideration all possible microcontroller features/effects
+            //that can affect the oscillator settings (such as internal/external 
+            //switchover (two speed start up), fail-safe clock monitor, PLL lock time,
+            //external crystal/resonator startup time (if using a crystal/resonator),
+            //etc.
+
+            //Additionally, the official USB specifications dictate that USB devices
+            //must become fully operational and ready for new host requests/normal 
+            //USB communication after a 10ms "resume recovery interval" has elapsed.
+            //In order to meet this timing requirement and avoid possible issues,
+            //make sure that all necessary oscillator startup is complete and this
+            //function has returned within less than this 10ms interval.
+
+            break;
+
+        default:
+            break;
+    }
+}
+
+#if defined(USB_INTERRUPT)
+void __attribute__((interrupt,auto_psv)) _USB1Interrupt()
+{
+    USBDeviceTasks();
+}
+#endif
